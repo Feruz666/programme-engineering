5984f477d6a5866d915fc440e1a075941a842da5
Feruz Kayumov <43008107+Feruz666@users.noreply.github.com>
2019-12-19 17:43:05 +0500

    Update welcome.blade.php
    added lalalala

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Develop
+                    Laravel Develop Lalalala
                 </div>
 
                 <div class="links">
