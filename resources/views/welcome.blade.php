bb50bed948577f6caa4bf2229cc21725f55203bd
Feruz Kayumov <43008107+Feruz666@users.noreply.github.com>
2019-12-19 16:40:20 +0500

    Update welcome.blade.php
    checking jenkins

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Testing
+                    Laravel Testing Checking changes
                 </div>
 
                 <div class="links">
