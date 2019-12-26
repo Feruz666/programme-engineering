b0e07a0d19dd8d3dc5fdcf9b7d8aca54c31e3e3d
AnushkaGl <48246465+AnushkaGl@users.noreply.github.com>
2019-12-26 17:17:48 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Gladysheva
+                    Gladysheva55555555555
                 </div>
 
                 <div class="links">
