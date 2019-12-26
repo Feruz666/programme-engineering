8e5d22cc08b3edf1f33c2b23f878d5e86450a2ba
AnushkaGl <48246465+AnushkaGl@users.noreply.github.com>
2019-12-26 14:13:35 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Develop
+                    Laravel annnnnnn
                 </div>
 
                 <div class="links">
