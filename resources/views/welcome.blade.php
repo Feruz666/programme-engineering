efc702a48eff504b993774aa5f84d35e80bcf3d5
brasko1998 <59234859+brasko1998@users.noreply.github.com>
2019-12-26 17:18:27 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   uvat foreever
+                   uvat
                 </div>
 
                 <div class="links">
